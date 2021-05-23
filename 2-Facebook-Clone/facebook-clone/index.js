686251ea8d1c3acb9845ad59c37bd205924eac4a
ThibaMahlezana <thiba.ma@gmail.com>
2021-05-23 16:05:58 +0200

    added posts model and routes

--- 2-Facebook-Clone/facebook-clone/index.js
+++ 2-Facebook-Clone/facebook-clone/index.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -39,6 +40,7 @@ app.use(morgan("common"));
 // routes
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 // runing the server
 app.listen(8800, () => {
